7981e168f7d13a2589b60324e101489c0c36f36e
Roger <roger.lampe@gmail.com>
2019-11-13 20:45:44 -0500

    Add solid storage

--- BigStorage/BigStorageMod.cs
+++ BigStorage/BigStorageMod.cs
@@ -24,6 +24,10 @@ namespace BigStorage
                 Strings.Add(BigLiquidStorage.DESC.key.String, BigLiquidStorage.DESC.text);
                 Strings.Add(BigLiquidStorage.EFFECT.key.String, BigLiquidStorage.EFFECT.text);
                 ModUtil.AddBuildingToPlanScreen("Base", BigLiquidStorage.ID);
+                Strings.Add(BigStorageLocker.NAME.key.String, BigStorageLocker.NAME.text);
+                Strings.Add(BigStorageLocker.DESC.key.String, BigStorageLocker.DESC.text);
+                Strings.Add(BigStorageLocker.EFFECT.key.String, BigStorageLocker.EFFECT.text);
+                ModUtil.AddBuildingToPlanScreen("Base", BigStorageLocker.ID);
             }
 
             private static void Postfix()
@@ -32,6 +36,8 @@ namespace BigStorage
                 BuildingConfigManager.Instance.RegisterBuilding(obj as IBuildingConfig);
                 object obj2 = Activator.CreateInstance(typeof(BigLiquidStorage));
                 BuildingConfigManager.Instance.RegisterBuilding(obj2 as IBuildingConfig);
+                object obj3 = Activator.CreateInstance(typeof(BigStorageLocker));
+                BuildingConfigManager.Instance.RegisterBuilding(obj3 as IBuildingConfig);
             }
 
             [HarmonyPatch(typeof(Db), "Initialize")]
@@ -43,6 +49,8 @@ namespace BigStorage
                     Techs.TECH_GROUPING["LiquidTemperature"] = ls.ToArray();
                     List<string> ls2 = new List<string>(Techs.TECH_GROUPING["Catalytics"]) { BigGasStorage.ID };
                     Techs.TECH_GROUPING["Catalytics"] = ls2.ToArray();
+                    List<string> ls3 = new List<string>(Techs.TECH_GROUPING["RefinedObjects"]) { BigStorageLocker.ID };
+                    Techs.TECH_GROUPING["RefinedObjects"] = ls3.ToArray();
                 }
             }
         }
