2a7963a144f03d5dd5a0399849be4987618ee30d
Roger <roger.lampe@gmail.com>
2022-01-30 18:09:53 -0500

    new colors in config

--- BigStorage/Config.cs
+++ BigStorage/Config.cs
@@ -14,5 +14,17 @@ namespace BigStorage
 		public float BigLiquidStorageCapacity { get; set; } = 25000f;
 		[JsonProperty]
 		public float BigBeautifulStorageLockerCapacity { get; set; } = 100000f;
+		[JsonProperty]
+		public byte Red { get; set; } = 0;
+		[JsonProperty]
+		public byte Green { get; set; } = 150;
+		[JsonProperty]
+		public byte Blue { get; set; } = 255;
+        [JsonProperty]
+		public byte BeautifulRed { get; set; } = 200;
+		[JsonProperty]
+		public byte BeautifulGreen { get; set; } = 0;
+		[JsonProperty]
+		public byte BeautifulBlue { get; set; } = 255;
 	}
 }
