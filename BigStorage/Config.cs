14bc67df1bb5fe80af9cb3d3a0c48f601c759d21
David <david.turley@live.nl>
2022-02-17 14:42:29 +0100

    Added a catagory.
    There is no way to order them yet, so the naming of the category is more for ordering in the mod menu than anything else.

--- BigStorage/Config.cs
+++ BigStorage/Config.cs
@@ -10,29 +10,29 @@ namespace BigStorage
     public class Config : SingletonOptions<Config>
     {
         // Capacity
-        [JsonProperty] [Option("Big Storage Locker Capacity (kg)", "Determines the capacity of the Big Storage Locker in kg.", Format = "F1")] [Limit(1000, 1000000)]
+        [JsonProperty] [Option("Big Storage Locker Capacity (kg)", "Determines the capacity of the Big Storage Locker in kg.", "Capacity", Format = "F1")] [Limit(1000, 1000000)]
         public int BigStorageLockerCapacity { get; set; } = 100000;
-        [JsonProperty] [Option("Big Beautiful Storage Locker Capacity (kg)", "Determines the capacity of the Big Beautiful Storage Locker in kg.", Format = "F1")] [Limit(1000, 1000000)]
+        [JsonProperty] [Option("Big Beautiful Storage Locker Capacity (kg)", "Determines the capacity of the Big Beautiful Storage Locker in kg.", "Capacity", Format = "F1")] [Limit(1000, 1000000)]
         public int BigBeautifulStorageLockerCapacity { get; set; } = 100000;
-        [JsonProperty] [Option("Big Gas Container Capacity (kg)", "Determines the capacity of the Big Gas Container in kg.", Format = "F1")] [Limit(100, 10000)]
+        [JsonProperty] [Option("Big Gas Container Capacity (kg)", "Determines the capacity of the Big Gas Container in kg.", "Capacity", Format = "F1")] [Limit(100, 10000)]
         public int BigGasLockerCapacity { get; set; } = 750;
-        [JsonProperty] [Option("Big Liquid Storage Capacity (kg)", "Determines the capacity of the Big Liquid Storage in kg.", Format = "F1")] [Limit(1000, 100000)]
+        [JsonProperty] [Option("Big Liquid Storage Capacity (kg)", "Determines the capacity of the Big Liquid Storage in kg.", "Capacity", Format = "F1")] [Limit(1000, 100000)]
         public int BigLiquidStorageCapacity { get; set; } = 25000; 
         
         // Colour
-        [JsonProperty] [Option("Red", "Set the RED tint for the Big Storages.")] [Limit(0, 255)]
+        [JsonProperty] [Option("Red", "Set the RED tint for the Big Storages.", "Colour Storage Locker")] [Limit(0, 255)]
         public int Red { get; set; } = 0;
-        [JsonProperty] [Option("Green", "Set the GREEN tint for the Big Storages.")] [Limit(0, 255)]
+        [JsonProperty] [Option("Green", "Set the GREEN tint for the Big Storages.", "Colour Storage Locker")] [Limit(0, 255)]
 		public int Green { get; set; } = 150;
-        [JsonProperty] [Option("Blue", "Set the BLUE tint for the Big Storages.")] [Limit(0, 255)]
+        [JsonProperty] [Option("Blue", "Set the BLUE tint for the Big Storages.", "Colour Storage Locker")] [Limit(0, 255)]
 		public int Blue { get; set; } = 255;
 
         //Colour Beautiful
-        [JsonProperty] [Option("Red (Beautiful)", "Set the RED tint for the Big Beautiful Storages.")] [Limit(0f, 255f)]
+        [JsonProperty] [Option("Red (Beautiful)", "Set the RED tint for the Big Beautiful Storages.", "Colour Beautiful Storage Locker")] [Limit(0f, 255f)]
 		public int BeautifulRed { get; set; } = 200;
-        [JsonProperty] [Option("Green (Beautiful)", "Set the GREEN tint for the Big Beautiful Storages.")] [Limit(0f, 255f)]
+        [JsonProperty] [Option("Green (Beautiful)", "Set the GREEN tint for the Big Beautiful Storages.", "Colour Beautiful Storage Locker")] [Limit(0f, 255f)]
 		public int BeautifulGreen { get; set; } = 0;
-        [JsonProperty] [Option("Blue (Beautiful)", "Set the BLUE tint for the Big Beautiful Storages.")] [Limit(0f, 255f)]
+        [JsonProperty] [Option("Blue (Beautiful)", "Set the BLUE tint for the Big Beautiful Storages.", "Colour Beautiful Storage Locker")] [Limit(0f, 255f)]
 		public int BeautifulBlue { get; set; } = 255;
 	}
 }
